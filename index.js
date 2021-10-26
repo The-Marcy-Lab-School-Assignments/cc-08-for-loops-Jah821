1203fd7857cbde0e58ec723f9a1bd503bc350c1d
Jah821 <91166820+Jah821@users.noreply.github.com>
2021-10-26 14:09:32 +0000

    I understand noww

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 //1
 function countToTwenty(){
-  
+  for (let i = 1; i <= 20; i++){
+      console.log(i)
+  }
 }
 
 //2
 function countFromTwenty(){
-  
+  for (let i = 20; i => 1; i--){
+      console.log(i)
+  }
 }
 
 //3
-function countToN(){
-  
+function countToN(number){
+  for (let i = 1; i <= number; i++){
+      console.log(i)
+  }
 }
 
 
 //4
-function countFromN(){
-  
+function countFromN(number){
+  for (let i = number; i => 1; i--){
+      console.log(i)
+  }
 }
 
 //5
-function countEveryOdd(){
-  
+function countEveryOdd(number){
+  for (let i = 1; i <= number; i+=2){
+      console.log(i)
+  }
 }
+
+countEveryOdd(20)
